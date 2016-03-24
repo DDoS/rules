5a2525c8d5c8b940b5deb1010800bebb1d7ad91b
Aleksi Sapon <aleksi@sapon.ca>
2016-03-23 23:17:22 -0400

    Improve lexing error messages

--- lang/source/app.d
+++ lang/source/app.d
@@ -1,6 +1,7 @@
 import std.stdio;
 
 import ruleslang.syntax.dcharstream;
+import ruleslang.syntax.token;
 import ruleslang.syntax.tokenizer;
 import ruleslang.syntax.ast.statement;
 import ruleslang.syntax.parser.statement;
@@ -9,21 +10,21 @@ import ruleslang.semantic.litreduce;
 
 void main() {
 	while (true) {
+		stdout.write("> ");
+		auto source = stdin.readln();
+		if (source.length <= 0) {
+			stdout.writeln();
+			break;
+		}
 		try {
-			stdout.write("> ");
-			auto source = stdin.readln();
-			if (source.length <= 0) {
-				stdout.writeln();
-				break;
-			}
 			auto tokenizer = new Tokenizer(new DCharReader(new StringDCharStream(source)));
 		    foreach (statement; tokenizer.parseStatements()) {
 				statement = statement.expandOperators();
 				statement = statement.reduceLiterals();
 				stdout.writeln(statement.toString());
 		    }
-		} catch (Exception exception) {
-			stdout.writeln(exception.msg);
+		} catch (SourceException exception) {
+			writeln(exception.getErrorInformation(source).toString());
 		}
 	}
 	// TODO: fix unsafe casts
--- lang/source/ruleslang/syntax/token.d
+++ lang/source/ruleslang/syntax/token.d
@@ -43,11 +43,13 @@ public interface SourceIndexed {
 }
 
 public class SourceException : Exception, SourceIndexed {
+    private string offender = null;
     private size_t _start;
     private size_t _end;
 
-    public this(string message, size_t index) {
+    public this(string message, dchar offender, size_t index) {
         super(message);
+        this.offender = offender.escapeChar().to!string();
         _start = index;
         _end = index;
     }
@@ -64,6 +66,81 @@ public class SourceException : Exception, SourceIndexed {
 
     @property public override size_t end() {
         return _start;
+    }
+
+    public immutable(ErrorInformation)* getErrorInformation(string source) {
+        import std.algorithm.comparison : min;
+        // find the line number the error occurred on
+        size_t lineNumber = findLine(source, min(_start, source.length - 1));
+        // find start and end of line containing the offender
+        long lineStart = _start, lineEnd = _start;
+        while (--lineStart >= 0 && !source[lineStart].isNewLineChar()) {
+        }
+        lineStart++;
+        while (++lineEnd < source.length && !source[lineEnd].isNewLineChar()) {
+        }
+        lineEnd--;
+        string line = source[lineStart .. min(lineEnd + 1, $)];
+        return new ErrorInformation(this.msg, offender, line, lineNumber, _start - lineStart, _end - lineStart);
+    }
+
+    private static size_t findLine(string source, size_t index) {
+        size_t line = 0;
+        for (size_t i = 0; i <= index; i++) {
+            if (source[i].isNewLineChar()) {
+                consumeNewLine(source, i);
+                line++;
+            }
+        }
+        return line;
+    }
+
+    private static void consumeNewLine(string source, ref size_t i) {
+        if (source[i] == '\n') {
+            // LF
+            i++;
+        } else if (source[i] == '\r') {
+            // CR
+            i++;
+            if (i < source.length && source[i] == '\n') {
+                // CR + LF
+                i++;
+            }
+        }
+    }
+
+    public immutable struct ErrorInformation {
+        public string message;
+        public string offender;
+        public string line;
+        public size_t lineNumber;
+        public size_t startIndex;
+        public size_t endIndex;
+
+        public string toString() {
+            char[] buffer = [];
+            buffer.reserve(256);
+            buffer ~= "Error: \"" ~ message ~ '"';
+            if (offender != null) {
+                buffer ~= " caused by '" ~ offender ~ '\'';
+            }
+            buffer ~= " at line: " ~ lineNumber.to!string ~ ", index: " ~ startIndex.to!string;
+            if (startIndex != endIndex) {
+                buffer ~= " to " ~ endIndex.to!string;
+            }
+            buffer ~= " in \n" ~ line ~ '\n';
+            foreach (i; 0 .. startIndex) {
+                buffer ~= ' ';
+            }
+            if (startIndex == endIndex) {
+                buffer ~= '^';
+            } else {
+                for (size_t i = startIndex; i <= endIndex; i++) {
+                    buffer ~= '~';
+                }
+            }
+            return buffer.idup;
+        }
     }
 }
 
