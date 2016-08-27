98485903d85f9750258258180f79ef9e45d7b8bf
Aleksi Sapon <aleksi@sapon.ca>
2016-08-27 17:17:37 -0400

    Better test error output

--- lang/test/source/ruleslang/test/assertion.d
+++ lang/test/source/ruleslang/test/assertion.d
@@ -2,14 +2,14 @@ module ruleslang.test.assertion;
 
 import std.format : format;
 
-public void assertEqual(T)(T a, T b) {
+public void assertEqual(T)(T a, T b, string file = __FILE__, size_t line = __LINE__) {
     static if (is(T == interface)) {
         bool equal = a.opEquals(b);
     } else {
         bool equal = a == b;
     }
     if (!equal) {
-        throw new AssertionError(format("%s != %s", a, b));
+        throw new AssertionError(format("%s != %s\nin %s line %d", a, b, file, line));
     }
 }
 
