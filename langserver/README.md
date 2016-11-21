a5139a463643ad75afd3c88d12784b6887005081
Michael Golfi <michael.m.golfi@gmail.com>
2016-11-21 00:50:11 -0500

    Commented out all attempts to create a shared state for rule sets. Reverted back to primitive approach.

--- langserver/README.md
+++ langserver/README.md
@@ -8,12 +8,17 @@ In order to test the server:
 
 ``` bash
 
+# Compile
+dub
+
+# Run
 ./langserver
 
+# Test
 curl -X POST -H "Content-Type: application/json" -d \
 '{
 	"input":"{\"anInt\": {\"i\": 2}, \"aFloat\": {\"f\": 7.5}}",
-	"rules":"def Numbers: {AnInt anInt, AFloat aFloat}\n\ndef AnInt: {sint64 i}\ndef AFloat: {fp32 f}\n\nwhen (Numbers numbers):\n    return numbers.anInt.i != numbers.aFloat.f;\n\nthen (Numbers numbers):\n    return {anInt: {i: numbers.anInt.i + sint64(numbers.aFloat.f)}, aFloat: numbers.aFloat}"
+	"source":"def Numbers: {AnInt anInt, AFloat aFloat}\n\ndef AnInt: {sint64 i}\ndef AFloat: {fp32 f}\n\nwhen (Numbers numbers):\n    return numbers.anInt.i != numbers.aFloat.f;\n\nthen (Numbers numbers):\n    return {anInt: {i: numbers.anInt.i + sint64(numbers.aFloat.f)}, aFloat: numbers.aFloat}"
  }' \ 
-"http://localhost:8080/interpret"
+"http://localhost:8080/api/v1/rules/:ruleset"
 ```
--- langserver/source/app.d
+++ langserver/source/app.d
@@ -1,6 +1,7 @@
 import vibe.d;
 import std.stdio;
 import std.json;
+import std.typecons : Rebindable;
 
 import ruleslang.syntax.dchars;
 import ruleslang.syntax.tokenizer;
@@ -21,15 +22,105 @@ import ruleslang.semantic.interpret;
 import ruleslang.evaluation.runtime;
 import ruleslang.evaluation.evaluate;
 import ruleslang.util;
+/*
+interface IRuleProvider
+{
+	@path("/api/v1/rules/:name") @method(HTTPMethod.POST)
+	void postRuleSet(string _name, string source);
 
-shared static RuleNode[string] rulesSets;
+	@path("/api/v1/rules/:name") @method(HTTPMethod.PUT)
+	string evalRuleSet(string _name, string source, string input);
+}
+
+class RuleProvider : IRuleProvider 
+{
+
+	private:
+		Rebindable!(immutable RuleNode)[string] ruleSetsByName;
+
+	public:
+		this() {
+
+		}
+		
+		override:
+
+			void postRuleSet(string name, string source)
+			{		
+				writeln(name);
+				auto context = new Context();
+
+				try {
+					auto optRule = name in ruleSetsByName;
+
+					if (optRule is null) {
+						writeln("Rule does not exist: Creating...");
+					} else {
+						writeln("Rule already exists: Updating...");
+					}
+
+					auto ruleNode = new Tokenizer(new DCharReader(source)).parseRule().expandOperators().interpret(context);
+					ruleSetsByName[name] = ruleNode;
+				
+				} catch (SourceException exception) {
+					writeln(exception.getErrorInformation(source).toString());
+				}
+			}
+
+			string evalRuleSet(string name, string source, string input)
+			{
+				auto context = new Context();
+				writeln(input);
+				writeln(source);
+				try {
+					auto ruleNode = new Tokenizer(new DCharReader(source)).parseRule().expandOperators().interpret(context);
+					auto jsonInput = parseJSON(input);
+					auto jsonOutput = ruleNode.runRule(jsonInput);
+					
+					if (jsonOutput.isNull) {
+						writeln("Rule not applicable");
+						return "Rule not applicable";
+					} else {
+						writeln("Applying Rule: " ~ name);
+						return jsonOutput.get().toString();
+					}				
+				} catch (SourceException exception) {
+					writeln(exception.getErrorInformation(source).toString());
+					return exception.getErrorInformation(source).toString();
+				}
+			}*/
+
+			/*Nullable!JSONValue evalRuleSet(string name, string input)
+			{
+				writeln("Interpret: "~name);
+				
+				auto optRule = name in rules.ruleSetsByName;
+
+				if (optRule !is null) { 
+					auto rule = *optRule;
+					auto jsonInput = parseJSON(input);
+					auto jsonOutput = rule.runRule(jsonInput);
+					
+					if (jsonOutput.isNull) {
+						writeln("Rule not applicable");
+						return Nullable!JSONValue();
+					} else {
+						writeln("Applying Rule: " ~ name);
+						return jsonOutput;
+					}
+				} else {
+					return Nullable!JSONValue();
+				}
+			}
+}*/
 
 shared static this()
 {
 	auto router = new URLRouter;
+	//router.registerRestInterface(new RuleProvider);
 	router
-		.post("/rules/:ruleset", &newRuleSet)
-		.put("/rules/:ruleset", &interpret);		
+		//.post("/api/v1/rules/:ruleset", &newRuleSet)
+		.put("/api/v1/rules/:ruleset", &interpret);		
 
 	auto settings = new HTTPServerSettings;
 	settings.port = 8080;
@@ -40,6 +131,28 @@ shared static this()
 	logInfo("Please open http://127.0.0.1:8080/ in your browser.");
 }
 
+
+void interpret(HTTPServerRequest req, HTTPServerResponse res)
+{
+	auto input = req.json["input"].get!string;
+	auto rules = req.json["source"].get!string;
+	
+    auto jsonInput = parseJSON(input);
+    auto source = rules;
+
+    auto context = new Context();
+    auto ruleNode = new Tokenizer(new DCharReader(source)).parseRule().expandOperators().interpret(context);
+    auto jsonOutput = ruleNode.runRule(jsonInput);
+    if (jsonOutput.isNull) {
+        writeln("Rule not applicable");
+		writeBody("Rule not applicable","application/json; charset=UTF-8");
+    } else {
+		auto output = jsonOutput.get();
+		res.writeBody(output.toString(),"application/json; charset=UTF-8");
+    }
+}
+
+/*
 void newRuleSet(HTTPServerRequest req, HTTPServerResponse res)
 {
 	writeln("New RuleSet: "~req.params["ruleset"]);
@@ -47,11 +160,25 @@ void newRuleSet(HTTPServerRequest req, HTTPServerResponse res)
 	auto ruleName = req.params["ruleset"];
 	auto source = req.json["rules"].get!string;
     auto context = new Context();
-    auto ruleNode = new Tokenizer(new DCharReader(source)).parseRule().expandOperators().interpret(context);
+
+	try {
+		auto optRule = ruleName in ruleSetsByName;
+
+		if (optRule is null) {
+			writeln("Rule does not exist: Creating...");
+		} else {
+			writeln("Rule already exists: Updating...");
+		}
+
+		auto ruleNode = new Tokenizer(new DCharReader(source)).parseRule().expandOperators().interpret(context);
+		ruleSetsByName[ruleName] = ruleNode;
 	
-	writeln("Saving Rules... "~req.params["ruleset"]);
-	rulesSets[ruleName] = cast(shared RuleNode)ruleNode;	
-	writeln("Saved Rules... "~req.params["ruleset"]);
+	} catch (SourceException exception) {
+    	writeln(exception.getErrorInformation(source).toString());
+		res.writeBody(exception.getErrorInformation(source).toString(),"application/json; charset=UTF-8");
+    }
+
+	res.writeBody("Rule created!");
 }
 
 void interpret(HTTPServerRequest req, HTTPServerResponse res)
@@ -59,15 +186,23 @@ void interpret(HTTPServerRequest req, HTTPServerResponse res)
 	writeln("Interpret: "~req.params["ruleset"]);
 	
 	auto ruleName = req.params["ruleset"];
-	auto ruleSet = cast(immutable RuleNode)rulesSets[ruleName];
-	auto input = req.json["input"].get!string;
-	auto jsonInput = parseJSON(input);
-    auto jsonOutput = ruleSet.runRule(jsonInput);
-    
-	if (jsonOutput.isNull) {
-        writeln("Rule not applicable");
-    } else {
-		auto output = jsonOutput.get();
-		res.writeBody(output.toString(),"application/json; charset=UTF-8");
-    }
-}
+	auto optRule = ruleName in ruleSetsByName;
+
+	if (optRule !is null) { 
+		auto rule = *optRule;
+		auto input = req.json["input"].get!string;
+		auto jsonInput = parseJSON(input);
+		auto jsonOutput = rule.runRule(jsonInput);
+		
+		if (jsonOutput.isNull) {
+			writeln("Rule not applicable");
+			res.writeBody("Rule not applicable!");
+		} else {
+			writeln("Applying Rule: " ~ ruleName);
+			auto output = jsonOutput.get();
+			res.writeBody(output.toString(),"application/json; charset=UTF-8");
+		}
+	} else { 
+		res.writeBody("Rule doesn't exist!");
+	}
+}*/
