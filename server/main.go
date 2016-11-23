79162f82ac387a6daa47a2c19d746050679c462f
Michael Golfi <michael.m.golfi@gmail.com>
2016-11-22 19:15:27 -0500

    Added log msg to signal server startup. Added glide config for dep management.

--- server/main.go
+++ server/main.go
@@ -1,24 +1,26 @@
 package main
 
 import (
-	"github.com/michael-golfi/log4go"
 	"net/http"
+
 	"github.com/gorilla/mux"
+	"github.com/michael-golfi/log4go"
 	"github.com/michael-golfi/rules/server/pipeline"
 	"github.com/rs/cors"
 )
 
 const (
-	rulesUri = "http://127.0.0.1:9090/api/v1/rules"
-	dir = "config/"
+	rulesURI = "http://127.0.0.1:9090/api/v1/rules"
+	dir      = "config/"
 )
 
 func main() {
 	router := mux.NewRouter()
 
-	pipeline := pipeline.NewPipelineHandler(rulesUri, dir)
+	pipeline := pipeline.NewPipelineHandler(rulesURI, dir)
 	pipeline.SetRoutes(router)
 
 	handler := cors.Default().Handler(router)
+	log4go.Info("Starting Ledgr on port 8080")
 	log4go.Crash(http.ListenAndServe(":8080", handler))
 }
